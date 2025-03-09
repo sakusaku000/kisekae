9a384e90e041233679ceef4f61c20e888620fa7e
sakusaku000 <harry.spncr@gmail.com>
2025-03-09 17:54:00 +0000

    increased ds faceplate row size

--- src/components/Scenes/KyaryFullScreen/DSRowContainer.vue
+++ src/components/Scenes/KyaryFullScreen/DSRowContainer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="absolute inset-0 -left-[1000px] -right-[1000px] flex flex-col gap-16 justify-center translate-y-[-100px]">
+    <div class="absolute inset-0 -left-[2000px] -right-[2000px] flex flex-col gap-16 justify-center translate-y-[-100px]">
         <DSRow id="1" :height="1" :total="20"/>
         <DSRow id="2" :height=".7" :total="30" :offset="-10" :reverse="true"/>
         <DSRow id="3" :height="1.2" :total="20" :offset="-10"/>
